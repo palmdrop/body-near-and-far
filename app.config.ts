9837076beafb0ed1d88d72c3ebe6789317a40554
palmdrop <anton@exlex.se>
2024-08-20 13:09:15 +0200

    fix: remove filter on mobile and fix build script

--- app.config.ts
+++ app.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   ssr: false, 
-  appRoot: "./src",
-  vite: {
-    server: false
-  },
   server: {
     baseURL: process.env.BASE_PATH,
     preset: "static",
